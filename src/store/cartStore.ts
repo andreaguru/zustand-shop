da824adbcba2db0be244e08db0c2cbbc077e9f73
andrea.rebusti_ippen <andrea.rebusti@ippen-digital.de>
2024-09-23 13:07:24 +0200

    update cart store

--- src/store/cartStore.ts
+++ src/store/cartStore.ts
@@ -4,6 +4,7 @@ import {Product} from "../types.ts";
 interface CartState {
     cart: Product[];
     addToCart: (product: Product) => void;
+    addToCartAsync: (product: Product) => void;
     removeFromCart: (productId: number) => void;
     clearCart: () => void;
 
@@ -12,6 +13,10 @@ interface CartState {
 export const useCartStore = create<CartState>((set) => ({
     cart: [],
     addToCart: (product: Product) => set((state) => ({cart: [...state.cart, product]})),
+    addToCartAsync: async (product: Product) => {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        set((state) => ({cart: [...state.cart, product]}));
+    },
     removeFromCart: (productId: number) => set((state) => ({cart: state.cart.filter((product: Product) => product.id !== productId)})),
     clearCart: () => set(() => ({cart: []})),
 }));
